6869e44834cff3b53681faa381b786fc49f39286
Brian <bketelsen@gmail.com>
2016-09-23 19:21:40 +0000

    add API and React Frontend

--- cmd/qpid/main.go
+++ cmd/qpid/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 	"github.com/hybridgroup/gobot"
 	"github.com/hybridgroup/gobot/api"
 	"github.com/hybridgroup/gobot/platforms/raspi"
@@ -12,6 +11,12 @@ func main() {
 
 	g := gobot.NewGobot()
 	r := raspi.NewRaspiAdaptor("raspi")
+
+  	server := api.NewAPI(g)
+  	server.Port = "4000"
+  	server.AddHandler(api.BasicAuth("bbq", "gopher"))
+  	server.Start()
+
 	errs := r.Connect()
 	if errs != nil {
 		panic(errs)
@@ -21,7 +26,7 @@ func main() {
 	if e != nil {
 		panic(e)
 	}
-	work := func() {
+/*	work := func() {
 		gobot.Every(1*time.Second, func() {
 			// get temperature reading
 			b, e := r.I2cRead(0x4d, 3)
@@ -35,19 +40,31 @@ func main() {
 			fmt.Println(c)
 		})
 	}
+*/
 	robot := gobot.NewRobot("raspi",
 		[]gobot.Connection{r},
 		[]gobot.Device{},
-		work,
+		nil,
 	)
-	g.AddRobot(robot)
-	go func(){
 
-  	server := api.NewAPI(g)
-  	server.Port = "4000"
-  	server.AddHandler(api.BasicAuth("bbq", "gopher"))
-  	server.Start()
-}()
+    	robot.AddCommand("say_hello", func(params map[string]interface{}) interface{} {
+    		return fmt.Sprintf("%v says hello!", robot.Name)
+    	})
+
+    	robot.AddCommand("get_temp", func(params map[string]interface{}) interface{} {
+
+			b, e := r.I2cRead(0x4d, 3)
+			if e != nil {
+				panic(e)
+			}
+			// 2nd byte is temp C * 5
+			// 3rd byte is temp over 127
+			c := b[1] / 5
+			c = c + b[2]
+			return c
+    	})
+	g.AddRobot(robot)
+
 	g.Start()
 
 }
