b1ade614e02cf129fd33075c5decd0ff1c3be4c0
Brian <bketelsen@gmail.com>
2016-09-23 18:45:45 +0000

    now with 100% more gobot goodness

--- cmd/qpid/main.go
+++ cmd/qpid/main.go
@@ -1,22 +1,27 @@
 package main
 
-import "fmt"
-import "golang.org/x/exp/io/i2c"
+import (
+
+	"fmt"
+        "github.com/hybridgroup/gobot/platforms/raspi"
+)
 
 func main() {
-	fmt.Println("hello PI!")
-	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x4d)
-	if err != nil {
-		panic(err)
-	}
-	b := make([]byte, 3)
-	err = d.Read(b)
-	if err != nil {
-		panic(err)
-	}
-	temp := b[1]
-	celcius := temp / 5 + b[2]
-	f := celcius*9/5 + 32
 
-	fmt.Println(temp, celcius, f)
+        r := raspi.NewRaspiAdaptor("raspi")
+	errs := r.Connect()
+	if errs != nil {
+		panic(errs)
+	}
+
+	e := r.I2cStart(0x4d)
+	if e != nil {
+		panic(e)
+	}
+	b, e := r.I2cRead(0x4d,3)
+	if e != nil {
+		panic(e)
+	}
+	c := b[1] / 5
+	fmt.Println(c)
 }
