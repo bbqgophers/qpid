de04d76d67a5ac9ccaa6927de240882b569f06ef
Brian <bketelsen@gmail.com>
2016-09-23 19:07:09 +0000

    convert to gobot metal

--- cmd/qpid/main.go
+++ cmd/qpid/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-
 	"fmt"
-        "github.com/hybridgroup/gobot/platforms/raspi"
+	"time"
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/api"
+	"github.com/hybridgroup/gobot/platforms/raspi"
 )
 
 func main() {
 
-        r := raspi.NewRaspiAdaptor("raspi")
+	g := gobot.NewGobot()
+	r := raspi.NewRaspiAdaptor("raspi")
 	errs := r.Connect()
 	if errs != nil {
 		panic(errs)
@@ -18,14 +21,33 @@ func main() {
 	if e != nil {
 		panic(e)
 	}
-	// get temperature reading
-	b, e := r.I2cRead(0x4d,3)
-	if e != nil {
-		panic(e)
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			// get temperature reading
+			b, e := r.I2cRead(0x4d, 3)
+			if e != nil {
+				panic(e)
+			}
+			// 2nd byte is temp C * 5
+			// 3rd byte is temp over 127
+			c := b[1] / 5
+			c = c + b[2]
+			fmt.Println(c)
+		})
 	}
-	// 2nd byte is temp C * 5
-	// 3rd byte is temp over 127
-	c := b[1] / 5
-	c = c + b[2]
-	fmt.Println(c)
+	robot := gobot.NewRobot("raspi",
+		[]gobot.Connection{r},
+		[]gobot.Device{},
+		work,
+	)
+	g.AddRobot(robot)
+	go func(){
+
+  	server := api.NewAPI(g)
+  	server.Port = "4000"
+  	server.AddHandler(api.BasicAuth("bbq", "gopher"))
+  	server.Start()
+}()
+	g.Start()
+
 }
