b678d047f3947651d21582b998a061fe56886d82
Brian Ketelsen <bketelsen@gmail.com>
2016-09-24 20:27:17 -0400

    fix calcs, add docs

--- gobot/probe.go
+++ gobot/probe.go
@@ -9,6 +9,7 @@ import (
 	"github.com/pkg/errors"
 )
 
+// GobotProbe is a thermocoupler connected to a raspberry pi
 type GobotProbe struct {
 	id          int
 	location    qpid.Location
@@ -21,6 +22,9 @@ type GobotProbe struct {
 	alerts      chan qpid.Notification
 }
 
+// NewProbe returns an initialized GobotProbe.
+// Location and description hard-coded for now, since
+// we only support one thermocoupler.
 func NewProbe(pi *raspi.RaspiAdaptor) *GobotProbe {
 
 	a := make(chan qpid.Notification)
@@ -33,6 +37,7 @@ func NewProbe(pi *raspi.RaspiAdaptor) *GobotProbe {
 	}
 }
 
+// Target is the temperature we'd like to reach
 func (g *GobotProbe) Target(temp qpid.Temp) (qpid.Temp, error) {
 	g.setpoint = temp
 	// todo get temp and return that instead
@@ -40,23 +45,33 @@ func (g *GobotProbe) Target(temp qpid.Temp) (qpid.Temp, error) {
 	return g.Temperature()
 }
 
+// Setpoint is the current Target
 func (g *GobotProbe) Setpoint() (qpid.Temp, error) {
 	return g.setpoint, nil
 }
 
+// HighThreshold is the temperature max before a critical
+// alert should be sent
 func (g *GobotProbe) HighThreshold(temp qpid.Temp) error {
 	g.high = temp
 	return nil
 }
 
+// LowThreshold is the temperature min before a critical alert
+// should be sent
 func (g *GobotProbe) LowThreshold(temp qpid.Temp) error {
 	g.low = temp
 	return nil
 }
 
+// Alerts returns a channel of notifications for probe
+// alerts
 func (g *GobotProbe) Alerts() chan qpid.Notification {
 	return g.alerts
 }
+
+// Temperature reads and returns the current temperature
+// from the probe
 func (g *GobotProbe) Temperature() (qpid.Temp, error) {
 	var t qpid.Temp
 	b, e := g.pi.I2cRead(i2cAddress, 3)
@@ -65,20 +80,25 @@ func (g *GobotProbe) Temperature() (qpid.Temp, error) {
 	}
 	// 2nd byte is temp C * 5
 	// 3rd byte is temp over 127
-	c := b[1] / 5
+	c := b[1] << 8
 	c = c + b[2]
+	c = c / 5
 	g.temperature = qpid.Temp(c)
 	return g.temperature, e
 }
 
+// Location returns the probe's location
 func (g *GobotProbe) Location() qpid.Location {
 	return g.location
 }
 
+// Description returns the probe's description
 func (g *GobotProbe) Description() string {
 	return g.description
 }
 
+// Source implements qpid.Sourcer and returns
+// the source of a notification
 func (g *GobotProbe) Source() string {
 	return fmt.Sprintf("Probe %d: %s", g.id, g.description)
 }
